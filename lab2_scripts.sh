3959ef9697bfd9f921a546bf3a6de289b5761fd9
Devinmagnuson <devinmagnuson1@gmail.com>
2019-02-03 17:40:36 -0700

    Updating lab2_scripts.sh and creating command_results.txt, email_results.txt, and phone_results.txt

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -3,4 +3,25 @@
 # Date: 1/30/2019
 
 #Problem 1 Code:
-#Make sure to document how you are solving each problem!
+#Make sure to document how you are solving each problem
+echo "Enter a regular expression: "
+read rEx
+echo "Enter a file name: "
+read fName
+
+phoneNumbers='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+
+echo "Total # of Phone Numbers: "
+grep -E $phoneNumbers $fName | wc -l
+
+grep -E '303-' $fName >> phone_results.txt
+
+
+echo "Total # of Emails: "
+grep -E '@' $fName | wc -l
+
+grep -E '@geocities.com' $fName >> email_results.txt
+
+grep -E $rEx $fName >> command_results.txt
+
+
